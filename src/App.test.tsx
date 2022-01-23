a5f379385229c39197182f6a76c344e121556369
monzoor-rm <monzoor.morshed@raremark.com>
2022-01-24 01:54:42 +0600

    Update readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./containers/Core";
+import { render, screen } from '@testing-library/react';
+import App from './containers/Core';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
